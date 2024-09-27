523c8a3b0e8aa45e56deff44a7de9c7ede3621b6
jangjian <pureunnabi@gmail.com>
2024-09-27 11:37:30 +0900

    로그인 버튼을 누르면 LoginActivity로 화면 전환

--- app/src/main/java/com/example/helloproject/MainActivity.java
+++ app/src/main/java/com/example/helloproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.helloproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button imageButton = (Button) findViewById(R.id.loginBtn);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
