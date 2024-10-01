465380a695b287d735cd6a84b5663a5344d02aa8
jangjian <pureunnabi@gmail.com>
2024-10-01 10:02:25 +0900

    회원가입 창, 로그인 폼 추가

--- app/src/main/java/com/example/helloproject/LoginActivity.java
+++ app/src/main/java/com/example/helloproject/LoginActivity.java
@@ -11,3 +11,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 }
+/*
+
+androidx.annotation.Nullable: 이 어노테이션은 해당 메서드가 null 값을 받을 수 있음을 나타냅니다.
+
+savedInstanceState는 액티비티의 이전 상태를 저장하고 복원하는 데 사용되는 데이터 묶음(Bundle)입니다.
+super.onCreate(savedInstanceState)는 부모 클래스인 AppCompatActivity의 onCreate() 메서드를 호출하여 기본적인 액티비티 초기화 작업을 수행합니다.
+*/
--- app/src/main/java/com/example/helloproject/MainActivity.java
+++ app/src/main/java/com/example/helloproject/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.helloproject;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,24 +16,26 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);         // 액티비티가 생성될 때 호출되는 메서드로, 앱의 초기 설정을 담당합니다
         setContentView(R.layout.activity_main);
 
-        Button imageButton = (Button) findViewById(R.id.loginBtn);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        Button loginButton = (Button) findViewById(R.id.loginBtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            public void onClick(View view) {    // onClick 메서드
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);   // 다른 액티비티나 앱 컨포넌트를 시작하는 데 사용
                 startActivity(intent);
             }
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button joinButton = (Button) findViewById(R.id.joinBtn);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
